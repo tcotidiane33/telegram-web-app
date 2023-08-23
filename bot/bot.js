46a8d09febb77d07f131a767d71eae5cbbca99f7
tcotidiane33 <tcotidiane33@gmail.com>
2023-08-23 18:37:07 +0000

    netlify deploy

--- bot/bot.js
+++ bot/bot.js
@@ -1,5 +1,4 @@
 const { Telegraf } = require("telegraf");
-//const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
 const TOKEN = "6445094207:AAE5AqlctyhsBUiQ0OMuM0mv0R4BfEl0TF8";
 const bot = new Telegraf(TOKEN);
 
