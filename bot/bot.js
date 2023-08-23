2570b4f679513bf1ade7eded53a5a35bc8785093
tcotidiane33 <tcotidiane33@gmail.com>
2023-08-23 11:19:45 +0000

    erreur WebApp import

---
+++ bot/bot.js
@@ -0,0 +1,16 @@
+const { Telegraf } = require("telegraf");
+//const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN = "6445094207:AAE5AqlctyhsBUiQ0OMuM0mv0R4BfEl0TF8";
+const bot = new Telegraf(TOKEN);
+
+const web_link = "https://https://www.matts.netlify.app/";
+
+bot.start((ctx) =>
+  ctx.reply("Welcome :) Chief <3 ", {
+    reply_markup: {
+      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+    },
+  })
+);
+
+bot.launch();
